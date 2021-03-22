a442ce7db196560b63db28a3189820a3617ccb78
herny.kim <hyeongjong90@gmail.com>
2021-03-23 01:02:21 +0900

    오늘 수정분 커밋

--- README.md
+++ README.md
@@ -9,6 +9,15 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 2. setState를 호출하면 리액트는 새로운 state와 함께 클래스 컴포넌트의 render 함수를 호출한다.
 
-## 3.2 동영상 시청해야함
-https://nomadcoders.co/react-fundamentals/lectures/1550
+
+## 3일차
+1. 리액트는 라이프 사이클이 존재한다.
+- componentDidMount() : render() 함수가 호출된 이후 호출.
+- componentWillUnmount() : 컴포넌트가 언마운트됨(페이지가 바뀐다거나 할때 호출됨).
+- componentWillMount() : DOM 노드에 추가하기 직전에 호출.
+2. state 를 필요로 하지 않는 컴포넌트의 경우에는 class 로 만들필요없이 function 으로 만들어도 된다.
+
+
+## 4.2 동영상 시청해야함
+https://nomadcoders.co/react-fundamentals/lectures/1557
 
--- src/App.js
+++ src/App.js
@@ -1,87 +1,47 @@
-import PropTypes from 'prop-types';
-import * as React from "react";
+import React from "react";
+import axios from "axios";
+import Movie from "./movie";
 
 class App extends React.Component {
 
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   }
 
-  /* setState를 호출할때마다 리액트는 새로운 state와 함께 render 함수를 호출한다. */
-  add = () => {
-    // Current 라는 함수를 제공해주기 때문에 State 에 접근 가능함.
-    this.setState(current => ({count: current.count + 1}));
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
   }
 
-  minus = () => {
-    // this.state 로 접근하는 방법은 좋지 않다. add 함수처럼 접근하는 것이 좋음.
-    this.setState({count: this.state.count - 1});
+  componentDidMount() {
+    this.getMovies();
   }
-
 
   render() {
+    const {isLoading, movies} = this.state;
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Plus</button>
-        <button onClick={this.minus}>Minus</button>
+        {
+          isLoading
+            ? 'Loading...'
+            : movies.map( movie => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image} />
+              )
+            })
+        }
       </div>
-
-    );
+    )
   }
+
 }
-
-
-
-// EXAMPLE
-/*
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h3>I like {name}</h3>
-      <h2>{rating} / 10 </h2>
-      <img src={picture} alt={name}/>
-    </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-const foodList = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'http://www.youngnong.co.kr/news/photo/201903/23371_17010_2150.png',
-    rating: 5
-  },
-  {
-    id: 2,
-    name: '김밥',
-    image: 'https://cdn.eyesmag.com/content/uploads/sliderImages/2020/03/04/ramen-and-gimbap-07-7d900d4b-8c20-4ff0-8f37-50579e73a152.jpg',
-    rating: 9
-  }
-];
-
-// Function Component 라고 부름
-function App() {
-  return (
-    <div>
-      {foodList.map(props => (
-        <Food
-          key={props.id}
-          name={props.name}
-          picture={props.image}
-          rating={props.rating}
-        />
-      ))}
-    </div>
-  )
-}
-*/
 
 export default App;
 
